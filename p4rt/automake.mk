1f6712e6a2d53bd139ad62c12721ee07810c3a2d
Tomasz Osinski <osinstom@gmail.com>
2020-04-11 15:31:34 +0200

    Remove dependencies on grpc

--- p4rt/automake.mk
+++ p4rt/automake.mk
@@ -1,5 +1,3 @@
-PROTOS = `ls -1 $(top_builddir)/p4rt/proto/*.proto`
-
 lib_LTLIBRARIES += p4rt/libp4rt.la
 
 p4rt_libp4rt_la_LDFLAGS = \
@@ -9,29 +7,10 @@ p4rt_libp4rt_la_LDFLAGS = \
 
 p4rt_libp4rt_la_SOURCES = \
     p4rt/p4rt.c \
-    p4rt/p4rt.h \
-	p4rt/proto/p4/v1/p4runtime.grpc-c.c \
-	p4rt/proto/p4/v1/p4runtime.grpc-c.h \
-	p4rt/proto/p4/v1/p4runtime.grpc-c.service.c
-
-p4runtime.grpc-c.c p4runtime.grpc-c.h p4runtime.grpc-c.service.c:
-	protoc -I $(top_builddir)/p4rt --grpc-c_out=../../../ \
-    		--plugin=protoc-gen-grpc-c=/usr/local/bin/protoc-gen-grpc-c \
-    	    $(top_builddir)/p4rt/proto/p4/v1/p4runtime.proto
+    p4rt/p4rt.h
 
 p4rt_libp4rt_la_CPPFLAGS = $(AM_CPPFLAGS)
 p4rt_libp4rt_la_CFLAGS = $(AM_CFLAGS)
 
-gencode:
-	protoc -I $(top_builddir)/p4rt/ --grpc-c_out=$(top_builddir)/p4rt/ \
-    		--plugin=protoc-gen-grpc-c=/usr/local/bin/protoc-gen-grpc-c \
-    		$(top_builddir)/p4rt/proto/p4/v1/p4runtime.proto
-
 pkgconfig_DATA += \
 	p4rt/libp4rt.pc
-
-LDADD = \
-    -lgrpc \
-    -lgpr \
-    -lprotobuf-c
-
